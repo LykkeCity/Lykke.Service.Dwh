1265b59d2b2f658ee88bd4ef94a3c3e3f1059210
Alexey Novichikhin <alexey.novichikhin@lykke.com>
2018-07-19 15:01:29 +0300

    add refit attributes

--- client/Lykke.Service.Dwh.Client/IDwhClient.cs
+++ client/Lykke.Service.Dwh.Client/IDwhClient.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using Refit;
 
 namespace Lykke.Service.Dwh.Client
 {
     public interface IDwhClient
     {
+        [Post("/api/StoredProcedures")]
         ResponceDataSet Get();
 
+        [Post("/api/StoredProcedures/call/{procname}")]
         ResponceDataSet Call(Dictionary<string, string> parameters, string procname);
 
+        [Post("/api/StoredProcedures/call/{procname}/{database}")]
         ResponceDataSet Call(Dictionary<string, string> parameters, string procname, string database);
     }
 }
